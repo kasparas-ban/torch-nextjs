493edd932db75ce43ab10e32ab2f20092c69b0b7
Kasparas <kasparas.baniulis@gmail.com>
2023-10-13 22:08:11 +0300

    Initial changes

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "Torch",
+  description:
+    "Take control of your time, work on your goals, and achieve your dreams with a community of other doists!",
+  url: "https://torch.com",
+  ogImage: "https://torch.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/torch",
+    github: "https://github.com/torch-nextjs",
+  },
+}
