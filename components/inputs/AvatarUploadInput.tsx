5e3115c678fe658dea9f2e9cb46f43eb49e77be7
kasparas-ban <kasparas.baniulis@gmail.com>
2023-11-02 08:42:52 +0200

    Upload changes

--- components/inputs/AvatarUploadInput.tsx
+++ components/inputs/AvatarUploadInput.tsx
@@ -1,6 +1,13 @@
 "use client"
 
-import { InputHTMLAttributes, ReactNode, useCallback, useState } from "react"
+import {
+  ChangeEventHandler,
+  InputHTMLAttributes,
+  ReactNode,
+  useCallback,
+  useRef,
+  useState,
+} from "react"
 import { motion } from "framer-motion"
 import { useDropzone } from "react-dropzone"
 import AvatarUpload from "@/public/icons/avatarUpload.svg"
@@ -10,26 +17,42 @@ export default function AvatarUploadInput({
 }: {
   inputProps: InputHTMLAttributes<HTMLInputElement>
 }) {
+  const avatarImageRef = useRef<HTMLImageElement | null>(null)
   const [avatar, setAvatar] = useState<File | null>(null)
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Do something with the files
     console.log(acceptedFiles)
-    setAvatar(acceptedFiles[0])
+    // setAvatar(acceptedFiles[0])
+
+    if (avatarImageRef.current) {
+      avatarImageRef.current.src = URL.createObjectURL(acceptedFiles[0])
+    }
   }, [])
 
   const { getRootProps, getInputProps, isDragActive, onDropAccepted } =
     useDropzone({ onDrop })
 
-  console.log(getInputProps())
+  // console.log(getInputProps())
+
+  // const onInputChange = (e: ChangeEventHandler<HTMLInputElement>) => {
+  //   getInputProps().onChange?(e)
+
+  // }
 
   return (
     <div {...getRootProps()}>
       <input {...inputProps} {...getInputProps()} />
       {/* {isDragActive ? <p>Upload</p> : <p>+</p>} */}
       <div className="h-28 w-28 cursor-pointer rounded-full bg-gray-300">
+        <img
+          id="avatar"
+          src=""
+          ref={avatarImageRef}
+          className="relative h-28 w-28 rounded-full"
+        />
         {onDropAccepted ? (
-          <img id="avatar" src="" />
+          <img id="avatar" src="" ref={avatarImageRef} />
         ) : (
           <motion.div
             className="group flex h-full items-center justify-center rounded-full"
