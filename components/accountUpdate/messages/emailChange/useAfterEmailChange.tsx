6629066a5636196ca2df94961fbaebf2c88a47d9
kasparas-ban <kasparas.baniulis@gmail.com>
2023-11-18 18:23:01 +0200

    Test 2

--- components/accountUpdate/messages/emailChange/useAfterEmailChange.tsx
+++ components/accountUpdate/messages/emailChange/useAfterEmailChange.tsx
@@ -6,32 +6,27 @@ import { useUser } from "@clerk/clerk-react"
 import { useUpdateUserEmail } from "@/api/hooks/useUser"
 
 export default function useAfterEmailChange() {
-  const { user } = useUser()
-  const searchParams = useSearchParams()
-  const isAfterEmailChangeSuccess = searchParams.get("emailChangeSuccess")
-
-  const { mutateAsync: updateEmail } = useUpdateUserEmail()
-
-  const handleUpdateEmail = async () => {
-    try {
-      const primaryEmailAddress = user?.emailAddresses.find(
-        email => email.id === user?.primaryEmailAddressId
-      )
-      const newEmailAddress = user?.emailAddresses.find(
-        email => email.id !== user?.primaryEmailAddressId
-      )
-
-      if (!primaryEmailAddress || !newEmailAddress)
-        throw new Error("Failed to update the email")
-
-      user?.update({ primaryEmailAddressId: newEmailAddress?.id })
-      updateEmail({ email: newEmailAddress?.emailAddress })
-    } catch (e) {
-      // TODO: show error
-    }
-  }
-
-  useEffect(() => {
-    if (isAfterEmailChangeSuccess) handleUpdateEmail()
-  }, [isAfterEmailChangeSuccess, user])
+  // const { user } = useUser()
+  // const searchParams = useSearchParams()
+  // const isAfterEmailChangeSuccess = searchParams.get("emailChangeSuccess")
+  // const { mutateAsync: updateEmail } = useUpdateUserEmail()
+  // const handleUpdateEmail = async () => {
+  //   try {
+  //     const primaryEmailAddress = user?.emailAddresses.find(
+  //       email => email.id === user?.primaryEmailAddressId
+  //     )
+  //     const newEmailAddress = user?.emailAddresses.find(
+  //       email => email.id !== user?.primaryEmailAddressId
+  //     )
+  //     if (!primaryEmailAddress || !newEmailAddress)
+  //       throw new Error("Failed to update the email")
+  //     user?.update({ primaryEmailAddressId: newEmailAddress?.id })
+  //     updateEmail({ email: newEmailAddress?.emailAddress })
+  //   } catch (e) {
+  //     // TODO: show error
+  //   }
+  // }
+  // useEffect(() => {
+  //   if (isAfterEmailChangeSuccess) handleUpdateEmail()
+  // }, [isAfterEmailChangeSuccess, user])
 }
