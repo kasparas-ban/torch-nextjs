b9e64753ea187e7ea03bc7f23c4c9d0a3cdf0ea3
kasparas-ban <kasparas.baniulis@gmail.com>
2023-12-16 19:44:37 +0200

    Fixed timer layout animations

--- components/timer/TimerContainer.tsx
+++ components/timer/TimerContainer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion"
 
 import useTimerForm from "./hooks/useTimerForm"
 import timerButtons from "./timerClock/TimerActionsButtons"
@@ -14,32 +14,36 @@ export default function TimerContainer() {
 
   return (
     <motion.div
+      layout
       className="max-[320px]:mx-2"
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ type: "tween" }}
     >
-      <AnimatePresence mode="popLayout">
-        <TimerFocusForm key="timer_focus_form" />
+      <LayoutGroup>
+        <AnimatePresence mode="popLayout">
+          <TimerFocusForm key="timer_focus_form" />
 
-        {focusOn && (
-          <motion.div
-            key={`${focusOn.type}_${focusOn.value}_info`}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 20 }}
-          >
-            <TimerFocusInfo key="timer_focus_info" focusOn={focusOn} />
-          </motion.div>
-        )}
+          {focusOn && (
+            <motion.div
+              layout
+              key={`${focusOn.type}_${focusOn.value}_info`}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+            >
+              <TimerFocusInfo key="timer_focus_info" focusOn={focusOn} />
+            </motion.div>
+          )}
 
-        <TimerClock key="timer_clock" />
+          <TimerClock key="timer_clock" />
 
-        <timerButtons.TimerActionButtons key="timer_action_buttons" />
-        <TimerSettingsModal>
-          <timerButtons.TimerSettingsButton />
-        </TimerSettingsModal>
-      </AnimatePresence>
+          <timerButtons.TimerActionButtons key="timer_action_buttons" />
+          <TimerSettingsModal>
+            <timerButtons.TimerSettingsButton />
+          </TimerSettingsModal>
+        </AnimatePresence>
+      </LayoutGroup>
     </motion.div>
   )
 }
